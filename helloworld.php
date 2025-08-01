092345c78a54dafcf2c48c857bc13ada8b9b45ad
enshee2231 <princyxettri@gmail.com>
2025-08-01 15:20:46 +1000

    conditions

--- helloworld.php
+++ helloworld.php
@@ -7,15 +7,17 @@
 </head>
 <body>
     <?php
-    $var1 = 3;
-    $var2 = 2;
-    $var3;
-    /* Arithmetic examples */
-    echo("Variable 1 is $var1 <br>");
-    echo("Variable 2 is $var2 <br>");
-    $var3 = $var1 + $var2;
-    echo("The result of addition is $var3
-    <br>");
+   $var1 = 3;
+   $var2 = 2;
+   $var3;
+   /* if example */
+   if ($var1 == $var2)
+   {
+   echo(" $var1 is the same as $var2 <br>");
+   } else
+   {
+   echo(" $var1 is NOT same as $var2 <br>");
+   }
     ?>
 </body>
 </html>
