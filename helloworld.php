6a8a7a1f0d014f4b0c068965b0b305e77c018050
enshee2231 <princyxettri@gmail.com>
2025-08-01 15:19:24 +1000

    variable add

--- helloworld.php
+++ helloworld.php
@@ -7,7 +7,15 @@
 </head>
 <body>
     <?php
-    echo ("Hello World");
+    $var1 = 3;
+    $var2 = 2;
+    $var3;
+    /* Arithmetic examples */
+    echo("Variable 1 is $var1 <br>");
+    echo("Variable 2 is $var2 <br>");
+    $var3 = $var1 + $var2;
+    echo("The result of addition is $var3
+    <br>");
     ?>
 </body>
 </html>
